66e4242caa0d40ff988136827d53db28a074a04b
cocolote <skiel.j.lopez@gmail.com>
2015-10-03 22:57:39 -0400

    Added some style on the home page pretty sweet style

--- public/js/electronic101.js
+++ public/js/electronic101.js
@@ -3,16 +3,11 @@
 $('#start-button a').hover(handelerIn, handelerOut);
 
 function handelerIn() {
-  $('.light-up').css({ 'color': '#d1423a',
-                       'text-shadow': '0 0 2px white, 0 0 20px #d1423a' });
+  $('.light-up').css({ 'text-shadow': '0 0 2px white, 0 0 20px #d1423a' });                       
 }
 
 function handelerOut() {
-  $('.light-up').css({ 'color': '#000',
-                       'text-shadow': 'none' });
-
-  $('#start-button a').css({ 'color': '#111',
-                             'text-shadow': 'none' });
+  $('.light-up').css({ 'text-shadow': 'none' });
 }
 
 // SETUP SOUND MANAGER ==================================
